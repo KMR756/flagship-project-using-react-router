bb823b8cd021ecf9782c075fd48ae6f8773edce0
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-04-19 23:43:45 +0600

    modified router

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
+import About from "../pages/About";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+    children: [
+      {
+        path: "about",
+        Component: About,
+      },
+    ],
   },
 ]);
